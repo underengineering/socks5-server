12868e1e1bae35b59bb9818355f4869f071c9167
underengineering <san4a852b@gmail.com>
2024-03-30 18:07:44 +0300

    feat: Add anonymous auth

--- main.go
+++ main.go
@@ -18,20 +18,31 @@ func main() {
 		port = port_env
 	}
 
-	store := socks5.StaticCredentials{
-		os.Getenv("PROXY_USER"): os.Getenv("PROXY_PASS"),
+	options := []socks5.Option{
+		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
+	}
+
+	username := os.Getenv("PROXY_USER")
+	password := os.Getenv("PROXY_PASS")
+	if username != "" {
+		log.Println("Using username/password auth")
+		store := socks5.StaticCredentials{
+			username: password,
+		}
+
+		options = append(options, socks5.WithAuthMethods([]socks5.Authenticator{socks5.UserPassAuthenticator{Credentials: store}}))
+		options = append(options, socks5.WithCredential(store))
+	} else {
+		log.Println("Using no auth")
+		options = append(options, socks5.WithAuthMethods([]socks5.Authenticator{socks5.NoAuthAuthenticator{}}))
 	}
 
 	// Create a SOCKS5 server
 	server := socks5.NewServer(
-		socks5.WithAuthMethods([]socks5.Authenticator{socks5.UserPassAuthenticator{Credentials: store}}),
-		socks5.WithCredential(store),
-		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
+		options...,
 	)
 
 	log.Println("Listening on", host+":"+port)
-
-	// Create SOCKS5 proxy on localhost port 8000
 	if err := server.ListenAndServe("tcp", host+":"+port); err != nil {
 		panic(err)
 	}
